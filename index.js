a6e68abb2829c8bfa0104862ccb4fdb22dd29bac
Mansi Agarwal <82286085+mansi0803@users.noreply.github.com>
2023-07-01 21:46:43 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const express=require('express');
+const bodyParser= require('body-parser');
+const nodemailer= require('nodemailer');
+const multer= require('multer');
+const cors=require('cors');
+const app=express();
+const upload = multer({dest:'uploads/'});
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors());
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+app.get('/',()=>{
+    resizeBy.send('welcome to my forma')
+})
+
+ app.post('/app/forma',upload.single('file'),(req,res)=>{
+    const { name, email, email2, body } = req.body;
+  const file = req.file;
+ 
+  const smtpTransport = nodemailer.createTransport({
+    service: 'Gmail',
+    port:465,
+    auth: {
+      user: '',
+      pass: '',
+    },
+  });
+   
+
+  const mailOptions = {
+    from: 'mansiagarwaldev@gmail.com',
+    to: `${email},${email2}`,
+    subject: 'New email from mansi',
+    text: `
+      Name: ${name}
+      Email: ${email}
+      Body: ${body}
+    `,
+    attachments: [
+      {
+        filename: file.originalname,
+        path: file.path,
+      },
+    ],
+  };
+
+  smtpTransport.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(500).json({ success: false });
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.json({ success: true });
+    }
+  });
+  smtpTransport.close();
+})
+
+const PORT= process.env.PORT||3001;
+
+app.listen(PORT,()=>{
+    console.log(`server starting at the port ${PORT}`);
+})
